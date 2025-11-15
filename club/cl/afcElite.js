f75acd5e7b688e24349f691ce0f5cd10844c3215
wcxpert <joedidittt@gmail.com>
2025-11-15 04:11:30 +0000

    Add files via upload

---
+++ club/cl/afcElite.js
@@ -0,0 +1,28 @@
+window.LEAGUE_afcElite = [
+  // East
+  { name: "Ulsan HD", country: "South Korea", short: "UHD", image: "clubs/afcElite/ulsan.png" },
+  { name: "Vissel Kobe", country: "Japan", short: "VKO", image: "clubs/afcElite/vissel.png" },
+  { name: "Gangwon", country: "South Korea", short: "GAN", image: "clubs/afcElite/gangwon.png" },
+  { name: "Machida Zelvia", country: "Japan", short: "MAC", image: "clubs/afcElite/machida.png" },
+  { name: "Shanghai Shenhua", country: "China", short: "SHS", image: "clubs/afcElite/shenhua.png" },
+  { name: "Seoul", country: "South Korea", short: "SEO", image: "clubs/afcElite/seoul.png" },
+  { name: "Johor Darul Ta'zim", country: "Malaysia", short: "JDT", image: "clubs/afcElite/jdt.png" },
+  { name: "Sanfrecce Hiroshima", country: "Japan", short: "SAN", image: "clubs/afcElite/sanfrecce.png" },
+  { name: "Melbourne City", country: "Australia", short: "MCY", image: "clubs/afcElite/melbourne.png" },
+  { name: "Chengdu Rongcheng", country: "China", short: "CHD", image: "clubs/afcElite/chengdu.png" },
+  { name: "Buriram United", country: "Thailand", short: "BUR", image: "clubs/afcElite/buriram.png" },
+  { name: "Shanghai Port", country: "China", short: "SHP", image: "clubs/afcElite/port.png" },
+  // West
+  { name: "Al Hilal", country: "Saudi Arabia", short: "HIL", image: "clubs/afcElite/hilal.png" },
+  { name: "Al-Ahli", country: "Saudi Arabia", short: "AHL", image: "clubs/afcElite/ahli.png" },
+  { name: "Shabab Al Ahli", country: "UAE", short: "SAH", image: "clubs/afcElite/shabab.png" },
+  { name: "Al Wahda", country: "UAE", short: "WAH", image: "clubs/afcElite/wahda.png" },
+  { name: "Tractor", country: "Iran", short: "TRA", image: "clubs/afcElite/tractor.png" },
+  { name: "Sharjah", country: "UAE", short: "SHJ", image: "clubs/afcElite/sharjah.png" },
+  { name: "Al-Ittihad", country: "Saudi Arabia", short: "ITT", image: "clubs/afcElite/ittihad.png" },
+  { name: "Al-Gharafa", country: "Qatar", short: "GHA", image: "clubs/afcElite/gharafa.png" },
+  { name: "Al Sadd", country: "Qatar", short: "SAD", image: "clubs/afcElite/sadd.png" },
+  { name: "Al-Duhail", country: "Qatar", short: "DUH", image: "clubs/afcElite/duhail.png" },
+  { name: "Al-Shorta", country: "Iraq", short: "SHO", image: "clubs/afcElite/shorta.png" },
+  { name: "Nasaf", country: "Uzbekistan", short: "NAS", image: "clubs/afcElite/nasaf.png" }
+];
